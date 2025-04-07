243c7b2b192bf2ac678815fb03e42c4703fbe8de
baudelotphilippe <philippe@baudelot.eu>
2025-04-07 15:31:38 +0200

    add form and logger

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 final class GlobalController extends AbstractController
 {
@@ -17,10 +25,23 @@ final class GlobalController extends AbstractController
     }
 
     #[Route('/contact', name: 'app_contact')]
-    public function contact(): Response
+    public function contact(Request $request, LoggerInterface $logger): Response
     {
+        
+        $logger->info("page contact new");
+
+         $form = $this->createForm(ContactType::class);
+
+         $form->handleRequest($request);
+         if ($form->isSubmitted() && $form->isValid()) {
+            $infos=$form->getData();
+            dd($infos);
+
+         }
+
         return $this->render('global/contact.html.twig', [
             'h1' => 'Nous contacter',
+            'form' =>$form
         ]);
     }
 
